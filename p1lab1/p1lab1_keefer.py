60d090026cbfb042b0a471169b721517c50e72c6
RKCTI110 <keeferr8268@student.faytechcc.edu>
2024-09-24 14:32:02 -0400

    Add files via upload

---
+++ p1lab1/p1lab1_keefer.py
@@ -0,0 +1,29 @@
+#P1LAB - My First Program
+#KeeferR
+#9/24/24
+
+print("Hello world!")
+print("Is this working?")
+print('the cow says "moo"')
+print("Newline\n is kind of like \n<br>\n")
+print("\tTabs space out \t\twords")
+print("\\ is the backslash")
+
+
+
+print("Hello!")
+#greet the user by the name
+#TODO: ask their name
+#print("Whats your name?")
+#print("Enter first name:")
+first = input("What's your first name? ")
+#print("Enter last name:")
+last = input("What's your last name? ")
+
+# say their name
+print("Nice to meet you, ", first, last)
+
+#ask them if they like stuff
+#print("What do you like?")
+topic = input("What stuff do you like? ")
+print("COOL! I like", topic, "too.")

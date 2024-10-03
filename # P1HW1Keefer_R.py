0c43f4f1516ef8883700047f2333d1c1111a94aa
RKCTI110 <keeferr8268@student.faytechcc.edu>
2024-10-03 18:43:44 +0000

    add int(input) when using numbers!!

---
+++ # P1HW1Keefer_R.py
@@ -0,0 +1,15 @@
+# Keefer_R
+# 10/3/2024
+# P1HW1
+# Homework
+
+num_interger = 7
+num_exponent = 3
+answer_equals = 343
+
+print('Enter an interger as the base value:', end = ' ') 
+num_interger = int(input())
+print('Enter an interger as the exponent:', end = ' ')
+num_exponent = int(input())
+print(num_interger, 'raised to the power of', num_exponent, 'is', answer_equals)
+

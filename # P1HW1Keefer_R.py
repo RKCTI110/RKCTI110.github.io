2cb818e64186889248de421abcd6151de25d34a5
RKCTI110 <keeferr8268@student.faytechcc.edu>
2024-10-05 04:56:07 +0000

    finish adding and subtracting

--- # P1HW1Keefer_R.py
+++ # P1HW1Keefer_R.py
@@ -13,8 +13,19 @@ print('Enter an interger as the base value:', end = ' ')
 num_interger = int(input())
 print('Enter an interger as the exponent:', end = ' ')
 num_exponent = int(input())
-print(num_interger, 'raised to the power of', num_exponent, 'is', answer_equals)
+print(num_interger, 'raised to the power of', num_exponent, 'is', answer_equals, '!!!')
 
 print('-----Adding and Subtracting-----')
 
+start_int = 10
+add_int = 6
+sub_int = 4
 print('Enter a starting interger:', end = ' ')
+start_int = int(input())
+print('Enter an integer to add:', end = ' ')
+add_int = int(input())
+print('Enter an integer to subtract:', end = ' ')
+sub_int = int(input())
+
+
+print('10 + 6 - 4 is equal to 12')

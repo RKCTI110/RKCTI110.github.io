df253d39df798948d83920e6915f3f61d4fbc902
RKCTI110 <keeferr8268@student.faytechcc.edu>
2024-10-08 18:42:14 +0000

    fist total attempt

--- Py2/P1HW2_keefer.py
+++ Py2/P1HW2_keefer.py
@@ -3,6 +3,7 @@
 # P1HW2
 # Creating a budget for trip
 
+
 print('🎃 Trip Calculator 🦇')
 
 budget = float(input("What is your travel budget? $"))
@@ -11,8 +12,26 @@ destination = input("Where are you going? ")
 #fuel
 fuel = float(input("How much do you need for gas both ways? "))
 #accommodation
+hotel = float(input("How much is the total (include taxes and fees) for hotel stay? "))
 #food
+food = float(input("Estimate how much you'll need for ALL meals during trip. Don't forget travel days! "))
+
+total = fuel + hotel + food
+
+print('🧛 ----------Travel Expenses---------- 🕸️')
 #add expenses
+print('Location:', destination)
+print('Initial Budget:', budget)
+
+print('Fuel:', fuel)
+print('Hotel:', hotel)
+print('Food:', food)
+
+print('Total Expenses:', fuel + hotel + food)
+print('Remaining Balance:', budget -  total)
+
+
+
 #sub expenses
 
 #results
